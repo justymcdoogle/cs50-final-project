e315021f54b9ac3e7cdc4ccfdc6f84c20cf5d5a9
plonky <nexusrepository@proton.me>
2025-12-28 17:18:57 +0700

    I might have forgotten to push for like.. the entire time I made this project. So here is basically the finish project LOL

--- helpers.py
+++ helpers.py
@@ -1,6 +1,7 @@
 from flask import redirect, session
 from functools import wraps
 
+# just copied this basically from problem set 9/finance lol
 def login_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
@@ -8,3 +9,22 @@ def login_required(f):
             return redirect("/login")
         return f(*args, **kwargs)
     return decorated_function
+
+def validate_parsed_data(parsed_data):
+    """Make sure data is what we want before it goes into the database"""
+    LINE_LENGTH = 75
+    VALID_SECTIONS = ["verse", "chorus", "bridge", "intro", "outro"]
+    for section_number in parsed_data:
+        section_type = parsed_data[section_number]['type'].lower()
+        if section_type not in VALID_SECTIONS:
+            return "Invalid section type"
+        for line_number in parsed_data[section_number]['line']:
+            line = parsed_data[section_number]['line'][line_number]
+            if not line['chords']:
+                return "Chords cannot be empty"
+            if len(line['chords']) > LINE_LENGTH:
+                return "Chord line too long"
+            if len(line['lyrics']) > LINE_LENGTH:
+                return "Lyrics line too long"
+            
+
